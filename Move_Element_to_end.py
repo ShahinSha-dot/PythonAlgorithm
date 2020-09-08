287a44ecbe2ac1e6996548ace8e0e8989f5f25bc
Shahin Sha <neoblaze24@gmail.com>
2020-09-09 03:29:57 +0530

    Create Move_Element_to_end.py

---
+++ Move_Element_to_end.py
@@ -0,0 +1,14 @@
+#This algorithm moves selected number in a list to the end
+
+def move_to_last(array,number):
+    i = 0
+    j = len(array)-1
+    while i<j:
+        while i<j and array[j] == number:
+            j -= 1
+            if array[i] == number:
+                array[i],array[j] = array[j],array[i]
+            i += 1
+        return array
+
+#code Below
